a9e39a0a231a800f9857aba88094769b2562a847
sarzhh <146794146+sarzhh@users.noreply.github.com>
2024-08-26 23:49:07 +0000

    Add logic for bubble sort

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,9 +1,28 @@
-def array
+def get_array
   array = []
   puts "Enter array"
 input = gets.chomp
 array = input.split.map(&:to_i)
 end
 
-bubble = array
+bubble = get_array
 puts bubble.inspect
+
+def bubble_sort (bubble)
+ (bubble.length - 1 ).times do |i|
+  swapped = false 
+  
+   (bubble.length - i - 1).times do |j|
+    if bubble[j] > bubble[j+1]
+      bubble[j], bubble[j+1] = bubble[j+1], bubble[j]
+      swapped = true
+    end
+   end
+   if swapped == false
+    break
+   end
+ end
+ bubble 
+end
+
+puts bubble_sort(bubble).inspect
